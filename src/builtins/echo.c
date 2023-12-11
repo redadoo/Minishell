8a56cec57165153de7fb26e18eeee2f33e50b19a
Fborroto <borrotofrancesco@gmail.com>
2023-12-11 15:15:09 +0100

    echo norminetted

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,27 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edoardo <edoardo@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fborroto <fborroto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:21:54 by edoardo           #+#    #+#             */
-/*   Updated: 2023/11/25 20:49:32 by edoardo          ###   ########.fr       */
+/*   Updated: 2023/12/11 15:13:55 by fborroto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../lib/minishell.h"
 
-void	echo(t_token **token)
+static void	echo_util(t_token *tmp)
 {
-	t_token	*tmp;
-	int		option;
-
-	option = 0;
-	tmp = (*token)->next;
-	if (tmp && ft_strcmp(tmp->str, "-n") == 0)
-	{
-		option = 1;
-		tmp = tmp->next;
-	}
 	while (tmp && (tmp->type == ARG || tmp->type == STOP))
 	{
 		if (tmp->type == STOP)
@@ -39,6 +29,21 @@ void	echo(t_token **token)
 			write(1, " ", 1);
 		tmp = tmp->next;
 	}
+}
+
+void	echo(t_token **token)
+{
+	t_token	*tmp;
+	int		option;
+
+	option = 0;
+	tmp = (*token)->next;
+	if (tmp && ft_strcmp(tmp->str, "-n") == 0)
+	{
+		option = 1;
+		tmp = tmp->next;
+	}
+	echo_util(tmp);
 	if (option == 0)
 		write(1, "\n", 1);
 }
